55ec31e262a00d89fc4309fcac775f2f522c8d29
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2023-05-17 19:02:00 +0600

    task_details function for find details task

--- tasks/views.py
+++ tasks/views.py
@@ -20,3 +20,8 @@ def task_create(request):
 def task_list(request):
     tasks = Task.objects.all()
     return render(request, "tasks/task_list.html", { "tasks": tasks,})
+
+# Retrieve a single task
+def task_detail(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    return render(request, "tasks/task_detail.html", { "task": task, })
