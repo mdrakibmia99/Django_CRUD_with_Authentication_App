7d87a729d29a4dba13b86167143f02d933eeb513
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2023-05-17 18:55:41 +0600

    create a status model

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Status(models.TextChoices):
+    UNSTARTED = 'u', "Not started yet"
+    ONGOING = 'o', "Ongoing"
+    FINISHED = 'f', "Finished"
