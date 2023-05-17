023bfe0e5fbe619aef5fa2bb5ce8f80f58f40fba
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2023-05-17 18:36:54 +0600

    create home page view

--- MyApp/views.py
+++ MyApp/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 # Create your views here.
+# Create your views here.
+def home(request):
+    if 'user' in request.session:
+        current_user = request.session['user']
+        param = {'current_user': current_user}
+        return render(request, 'base.html', param)
+    else:
+        return redirect('login')
+    return render(request, 'login.html')
