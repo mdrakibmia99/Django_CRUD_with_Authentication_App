c591239249789e463bd284684fe1c065e104cff4
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2023-05-17 19:00:19 +0600

    crate a task create function

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
+from .models import Task
+from .forms import TaskForm
 
-# Create your views here.
+# Create a task
+def task_create(request):
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse("tasks:task_list"))
+    else:
+        form = TaskForm()
+
+    return render(request, "tasks/task_form.html", { "form": form, })
